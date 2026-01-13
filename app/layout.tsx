c642ced96f51b4fe6a1385887645ed3dac115342
andrew njoo <adnjoo@gmail.com>
2026-01-12 19:10:28 -0600

    Add footer component with GitHub and Godot Engine links

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { RoadmapProvider } from "@/lib/store/RoadmapContext";
 import { Header } from "@/components/layout/Header";
+import { Footer } from "@/components/layout/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-1">{children}</main>
+            <Footer />
           </div>
         </RoadmapProvider>
       </body>
